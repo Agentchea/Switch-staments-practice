d868a49d4a457412c826c8b19bfb38f21bc87770
Derrick Chea <derrick.c.chea@gmail.com>
2018-09-17 04:42:19 -0400

    Add files via upload

---
+++ switch2.js
@@ -0,0 +1,19 @@
+var apple = "fruit";
+var steak = "protein";
+var pasta = "starch";
+
+switch (steak) {
+  case "protein";
+  console.log("This is meat and is good");
+  break;
+
+  case "starch";
+console.log("this is for pasta and is for sauce");
+  break;
+
+  case "fruit";
+  console.log("this is an apple and is great");
+  break;
+  default:
+  console.log(apple);
+}
