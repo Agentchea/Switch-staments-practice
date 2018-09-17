89348403aa98e813245a065985c3577594f7f2dd
Derrick Chea <derrick.c.chea@gmail.com>
2018-09-17 04:12:36 -0400

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,16 @@
+var gender = "male";
+
+switch (gender) {
+  case "transgender";
+  document.write("this is someone who is no longer their orginal gender");
+  break;
+case "male";
+document.write("This is a man");
+break;
+
+case "female";
+document.write("this is a woman");
+break;
+default:
+document.write("You must be a robot then! gotcha!");
+}
